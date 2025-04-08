344a947dd01d88a5f27c84a41cd7230f38efcab4
osuossu8 <osuosuossu18@gmail.com>
2025-04-09 00:08:45 +0900

    fix

--- fastapi-psql-sqlalchemy-alembic-trigger-function-practice/api/main.py
+++ fastapi-psql-sqlalchemy-alembic-trigger-function-practice/api/main.py
@@ -20,19 +20,19 @@ def get_task_repository(db=Depends(get_db_session)) -> TaskRepositoryInterface:
 
 @app.post("/tasks/", response_model=Task)
 def create_task(
-    task: Task, task_repo: TaskRepositoryInterface = Depends(get_task_repository)
+    task: Task, task_repository: TaskRepositoryInterface = Depends(get_task_repository)
 ) -> Task:
-    existing_task = task_repo.get_task_by_title(title=task.title)
+    existing_task = task_repository.get_task_by_title(title=task.title)
     if existing_task:
         raise HTTPException(status_code=400, detail=f"task : {task.title} already exists.")
-    return task_repo.create_task(task=task)
+    return task_repository.create_task(task=task)
 
 
 @app.get("/tasks/{task_id}", response_model=Task)
 def get_task(
-    task_id: int, task_repo: TaskRepositoryInterface = Depends(get_task_repository)
+    task_id: int, task_repository: TaskRepositoryInterface = Depends(get_task_repository)
 ) -> Task:
-    task = task_repo.get_task_by_id(task_id=task_id)
+    task = task_repository.get_task_by_id(task_id=task_id)
     if not task:
         raise HTTPException(status_code=404, detail=f"task with id {task_id} not found.")
     return task
@@ -42,21 +42,21 @@ def get_task(
 def get_tasks(
     skip: int = 0,
     limit: int = 100,
-    task_repo: TaskRepositoryInterface = Depends(get_task_repository),
+    task_repository: TaskRepositoryInterface = Depends(get_task_repository),
 ) -> list[Task]:
-    return task_repo.get_tasks(skip=skip, limit=limit)
+    return task_repository.get_tasks(skip=skip, limit=limit)
 
 
 @app.post("/tasks/{task_id}", response_model=Task)
 def update_task(
     task_id: int,
     task: Task,
-    task_repo: TaskRepositoryInterface = Depends(get_task_repository),
+    task_repository: TaskRepositoryInterface = Depends(get_task_repository),
 ) -> Task:
-    existing_task = task_repo.get_task_by_id(task_id=task_id)
+    existing_task = task_repository.get_task_by_id(task_id=task_id)
     if not existing_task:
         raise HTTPException(status_code=404, detail=f"task with id {task_id} not found.")
-    return task_repo.update_task(task=task, task_id=task_id)
+    return task_repository.update_task(task=task, task_id=task_id)
 
 
 if __name__ == "__main__":
